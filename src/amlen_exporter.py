ba771b7ce00632176085f7aed51e707495c00d56
nlopatin <nlopatin@reksoft.ru>
2023-07-20 05:53:29 +0000

    convert boolean labels to string

--- src/amlen_exporter.py
+++ src/amlen_exporter.py
@@ -140,15 +140,13 @@ class JsonSubscriptionCollector():
         # "MessagingPolicy":"DemoTopicPolicy }] }
         try:
             for subscription in response['Subscription']:
-                is_durable = subscription['IsDurable']
-                is_shared = subscription['isShared']
                 labels = {
                     'MessagingPolicy': subscription['MessagingPolicy'],
                     'ClientID': subscription['ClientID'],
                     'SubName': subscription['SubName'],
                     'TopicString': subscription['TopicString'],
-                    'IsDurable': f'{is_durable}',
-                    'IsShared': f'{is_shared}'
+                    'IsDurable': str(subscription['IsDurable']),
+                    'IsShared': str(subscription['isShared'])
                 }
                 metric.add_sample('amlen_subscription_message_published', labels,
                                   subscription['PublishedMsgs'])
