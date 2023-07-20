b26be68bb193c46acb9bb7d96ece044f77469510
nlopatin <nlopatin@reksoft.ru>
2023-07-20 05:44:37 +0000

    pylint

--- src/amlen_exporter.py
+++ src/amlen_exporter.py
@@ -140,15 +140,15 @@ class JsonSubscriptionCollector():
         # "MessagingPolicy":"DemoTopicPolicy }] }
         try:
             for subscription in response['Subscription']:
-                isDurable = subscription['IsDurable']
-                isShared = subscription['isShared']
+                is_durable = subscription['IsDurable']
+                is_shared = subscription['isShared']
                 labels = {
                     'MessagingPolicy': subscription['MessagingPolicy'],
                     'ClientID': subscription['ClientID'],
                     'SubName': subscription['SubName'],
                     'TopicString': subscription['TopicString'],
-                    'IsDurable': f'{isDurable}',
-                    'IsShared': f'{isShared}'
+                    'IsDurable': f'{is_durable}',
+                    'IsShared': f'{is_shared}'
                 }
                 metric.add_sample('amlen_subscription_message_published', labels,
                                   subscription['PublishedMsgs'])
